604f3ee8f062a3938c0745b6da3400d70e684147
komothecat <afiqquraisyzulkarnain@gmail.com>
2024-01-19 21:54:34 +0800

    format: reformat bakefile with shfmt

--- Bakefile.sh
+++ Bakefile.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 task.dev() {
-    mkdir .luals
-    declare -A submods=(
-        [luasocket]=https://github.com/LuaCATS/luasocket
-        [lua-cjson]=https://github.com/goldenstein64/lua-cjson-definitions 
-    )
+  mkdir .luals
+  declare -A submods=(
+    [luasocket]=https://github.com/LuaCATS/luasocket
+    [lua - cjson]=https://github.com/goldenstein64/lua-cjson-definitions
+  )
 
-    for k in "${!submods[@]}"; do
-        v="${submods[$k]}"
-        git clone "$v" ".luals/$k"
-    done
+  for k in "${!submods[@]}"; do
+    v="${submods[$k]}"
+    git clone "$v" ".luals/$k"
+  done
 
-    luarocks init
-    luarocks install --deps-only ./lpresence-dev-1.rockspec
+  luarocks init
+  luarocks install --deps-only ./lpresence-dev-1.rockspec
 }
